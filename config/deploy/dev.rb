d1517441cd8a76106c67fb774e9643db9401cb32
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2017-11-01 21:46:07 +0100

    Added capistrano-docker port range constant

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,4 +2,6 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 
+:docker_compose_port_range, 80..90
+
 server "164.132.102.62", user: "aitor", roles: %w{app web}
