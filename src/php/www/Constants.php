591422adc598cfa7560515ea42d9e0a76e56d867
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2017-11-06 20:53:37 +0100

    * Added UnderstandMeCommand that basically analyze the text using Bluemix Watson service * Created a shared PHP file for constants * Added Guzzle library to make queries.

---
+++ src/php/www/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bilbot;
+
+
+class Constants
+{
+    const WATSON_API_USERNAME = '79dd7a9c-db38-4935-9711-31616cd2c9cf';
+    const WATSON_API_PASSWORD = 'bo6OJeiraQBp';
+    const WATSON_API_ENDPOINT = 'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/';
+}
