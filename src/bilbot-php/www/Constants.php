8e96aeba8befedad79e36dc2fc360bcab24def67
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2017-11-09 00:52:08 +0100

    Refactored Bilbot to Object Oriented PHP

---
+++ src/bilbot-php/www/Constants.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bilbot;
+
+
+class Constants
+{
+    const WATSON_API_USERNAME = '79dd7a9c-db38-4935-9711-31616cd2c9cf';
+    const WATSON_API_PASSWORD = 'bo6OJeiraQBp';
+    const WATSON_API_ENDPOINT = 'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/';
+
+    const TELEGRAM_API_USERNAME = 'bilb0_bot';
+    const TELEGRAM_API_KEY = '461265581:AAEGFfw4nQGquZWFcncGDPsiuJfTF3HbLvU';
+    const TELEGRAM_COMMANDS_PATH = '/Commands/';
+    const TELEGRAM_UPLOADS_PATH = '';
+    const TELEGRAM_DOWNLOADS_PATH = '';
+    const TELEGRAM_ERROR_LOGS_PATH = 'bilbot_errors.log';
+    const TELEGRAM_DEBUG_LOGS_PATH = 'bilbot_debug.log';
+
+    const MYSQL_HOST = 'bilbotpfm_bilbot-mysql_1';
+    const MYSQL_USER = 'puppy';
+    const MYSQL_PASSWORD = '1234';
+    const MYSQL_DATABASE = 'bilbot';
+}
