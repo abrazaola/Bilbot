f0f711220fa0a330f6a70270bab841610449c320
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2017-11-19 11:50:13 +0100

    Prepared Bilbot-WeLive to make WeLive API calls

--- src/bilbot-welive/src/AppBundle/Controller/REST/GatewayController.php
+++ src/bilbot-welive/src/AppBundle/Controller/REST/GatewayController.php
@@ -5,21 +5,16 @@ namespace AppBundle\Controller\REST;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class GatewayController extends FOSRestController
 {
-    public function understandMeAction(Request $request) {
-        $text = $request->get('text');
-
-        if (null === $text) {
-            return new JsonResponse(['error' => 'Empty text'], 300);
-        }
-
-        $res = $this->get('watson_api_consumer')->understandme($text);
+    public function bilbaoClubsAction() {
+        $res = $this
+            ->get('welive_api_consumer')
+            ->query('guia-de-asociaciones-de-bilbao', '2fb58eaa-3815-4b5f-bce7-91b9ec827640');
 
         $data = [
-            'analysis'    => $res,
+            'results'    => $res,
         ];
 
         return new JsonResponse($data, 200);
---
+++ src/bilbot-welive/src/AppBundle/DependencyInjection/WeLiveApiConsumer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use GuzzleHttp\Client;
+
+class WeLiveApiConsumer
+{
+    private $endpoint;
+
+    public function __construct(
+        $endpoint
+    )
+    {
+        $this->endpoint = $endpoint;
+    }
+
+    private function buildAddress($dataset, $resource) {
+        return $this->endpoint . $dataset . '/resource/' . $resource . '/query?origin=ANY';
+    }
+
+    public function query($dataset, $resource)
+    {
+        $client = new Client(
+            ['base_uri' =>
+                $this->buildAddress(
+                $dataset,
+                $resource)
+            ]
+        );
+
+        $res = $client->post(
+            '',
+            [
+                'Content-Type' => 'Content-Type: text/plain',
+                'Accept' => 'application/json',
+                'body' => 'select * from results limit 10;'
+            ]);
+
+        return json_decode($res->getBody());
+    }
+}
