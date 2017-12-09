c64390f6bf1d334cf172a5844f4b425b1d5ad7cf
Aitor Brazaola <aitor.brazaola@opendeusto.es>
2017-12-09 12:07:57 +0100

    Uncapistranized

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-group :development do
-  gem 'capistrano', '~> 3.10'
-  gem 'capistrano-rvm'
-  gem 'capistrano-composer'
-  gem 'capistrano-bundler'
-  gem 'capistrano-docker-compose'
-end
--- config/deploy/dev.rb
+++
@@ -1,7 +0,0 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-
-set :docker_compose_port_range, 80..90
-
-server "164.132.102.62", user: "aitor", roles: %w{app web}
